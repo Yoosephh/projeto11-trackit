1002b5a61f5790d5483ab012c3122647e27a7b1d
José Eduardo <jose.es.oleg@gmail.com>
2023-05-31 12:42:45 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import {GlobalStyleReset} from "./style/reset"
+
+export default function App() {
+
+  return (
+    <>
+      <GlobalStyleReset />
+      <h1>Hello world!</h1>
+    </>
+  )
+}
+
+
