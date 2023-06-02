9e0fa410346fa4fe610b8013991bb27be7c89613
José Eduardo <jose.es.oleg@gmail.com>
2023-06-02 15:54:40 -0300

    Corrige imagem 9

--- src/pages/RegisterPage/Register.jsx
+++ src/pages/RegisterPage/Register.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import styled from "styled-components"
-import { logo } from "../../../public/images/imgs"
+import  logo  from "../../../public/images/imgs"
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import CustomButton from "../../components/CustomButton";
